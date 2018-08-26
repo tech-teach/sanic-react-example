3d3c1c2d438175d463ebbb2bbc11abf21ff4cc69
dsvalenciah <dsvalenciah@unal.edu.co>
2018-08-26 14:13:17 -0500

    Code refactor

--- backend/app.py
+++ backend/app.py
@@ -14,6 +14,12 @@ async def save_task(request):
         return response.json({})
 
     task = request.json.get('task')
+    if not task:
+        return response.json(
+            {"status": "Task not found! please send me a task!"},
+            status=400
+        )
+
     tasks.append([task,False])
     return response.json({"status": "saved"})
 
@@ -22,12 +28,19 @@ async def delete_task(request, _id):
     if request.method == "OPTIONS":
         return response.json({})
 
-    if int(_id) >= len(tasks) or int(_id) < 0:
-        return response.json({"status": "not found"})
     try:
         del tasks[int(_id)]
     except ValueError:
-        return response.json({"status": "id is not valid"})
+        return response.json(
+            {"status": "The id param is not int parseable"},
+            status=400
+        )
+    except IndexError:
+        return response.json(
+            {"status": "Index out of bounds"},
+            status=400
+        )
+
     return response.json({"status": "deleted", "id": _id})
 
 @app.route('/tasks', methods=["GET", "OPTIONS"])
@@ -42,15 +55,23 @@ async def mark_as_done(request, _id):
     if request.method == "OPTIONS":
         return response.json({})
 
-    if int(_id) >= len(tasks) or int(_id) < 0:
-        return response.json({"status": "not found"})
     try:
-        if tasks[int(_id)][1]:
-            tasks[int(_id)][1]=False
+        id_index = int(_id)
+        if tasks[id_index][1]:
+            tasks[id_index][1]=False
         else:
-            tasks[int(_id)][1]=True
+            tasks[id_index][1]=True
     except ValueError:
-        return response.json({"status": "id is not valid"})
+        return response.json(
+            {"status": "The id param is not int parseable"},
+            status=400
+        )
+    except IndexError:
+        return response.json(
+            {"status": "Index out of bounds"},
+            status=400
+        )
+
     return response.json({"status": "realized", "id": _id})
 
 if __name__ == '__main__':
